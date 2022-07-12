ad754c85622c7ea8e205487864d39a17d986548f
Mhiroto7 <mhiroto.ai@gmail.com>
2022-07-12 22:43:59 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Streamlit 超入門')
+
+st.write('DataFreame')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+
+df = pd.DataFrame(
+     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+     columns=['lat', 'lon'])
+
+st.map(df)
